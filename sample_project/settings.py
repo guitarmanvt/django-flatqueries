4e572e58bc2c134e435b9a160cc32bd0548807ae
john@andersoninnovative.com <john@andersoninnovative.com@ea89a965-abb0-e77e-b502-39e82b801466>
2012-03-10 19:16:00 +0000

    Added demo data fixture and note in README.txt

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -14,7 +14,7 @@ PROJECT_ROOT = './'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_ROOT + 'flatqueries.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': PROJECT_ROOT + 'demo.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
